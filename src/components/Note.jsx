7a9adf36a843e443749e30c2f73c4b60f6f21204
florfu <97113940+florfu@users.noreply.github.com>
2022-05-28 15:19:05 -0300

    Add initial skeleton for the app

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Note title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+    );
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from "./components/App"
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <h1>Hello World!</h1>
+  <App />
 );
